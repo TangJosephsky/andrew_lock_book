aced92ace9cd130722c615a0bb31e477ef50a83b
Yaoxin Xiang <28193915+xiangyx@users.noreply.github.com>
2026-09-01 20:24:56 +0200

    extend webapp1

--- 03-create-your-first-aspdotnet-core-applications/WebApplication1/Program.cs
+++ 03-create-your-first-aspdotnet-core-applications/WebApplication1/Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.AspNetCore.HttpLogging;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpLogging(opts =>
+    opts.LoggingFields = HttpLoggingFields.RequestProperties);
+
+builder.Logging.AddFilter(
+    "Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpLogging();
+}
+
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/person", () => new Person("Yaoxin", "Xiang"));
 
 app.Run();
+
+public record Person(string FirstName, string LastName);
